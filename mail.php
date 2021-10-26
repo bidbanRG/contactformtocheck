58ceefe1b249894da8c8694e0fb447e623c1b74b
bidbanRG <77847748+bidbanRG@users.noreply.github.com>
2021-10-26 14:19:41 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,17 +1,18 @@
 <?php
 //get data from form  
-$name = $_POST['fname'];
+$fname = $_POST['fname'];
 $lname = $_POST['lname'];
 $message= $_POST['message'];
 $adress= $_POST['adress'];
 $to = "bideshbanerjee444@gmail.com";
+$email = "bideshbanerjee555@gmail.com"
 $subject = "Mail From website";
-$txt ="First Name = ". $name . "\r\n Last Name  = " . $lname . "\r\n Adress  = " . $adress ."\r\n Message =" . $message;
+$txt ="First Name = ". $fname . "\r\n Last Name  = " . $lname . "\r\n Adress  = " . $adress ."\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:thankyou.html");
+   header("Location:DOM1.html");
 ?>
